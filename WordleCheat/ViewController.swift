bf30b23441a2da94c7812a36213d161d5bb43d3b
owen-flanagan <flanagow@bc.edu>
2022-04-25 18:34:16 -0400

    maybe some of this does something

--- WordleCheat/ViewController.swift
+++ WordleCheat/ViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let letterFrequencies = ["A":43, "B": 11, "C":23, "D": 17, "E": 57, "F": 9, "G": 13, "H": 15, "I":38, "J":1,"K":6, "L":28, "M":15, "N":34, "O":37, "P":16, "Q":1, "R":39, "S":29, "T":35, "U":19, "V":5, "W":7, "X":1, "Y":9, "Z":1]
-    //https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +18,53 @@ class ViewController: UIViewController {
         wordList = s.components(separatedBy: "\n")
         print ("wordList count = \(wordList.count)")
         print(wordList)
+        
+        let remainingWords = remainingWord()
+        remainingWords.setWordList(words: wordList)
+        
+        
+        
     }
     
 
-
 }
 
+public extension String {
+  subscript(value: Int) -> Character {
+    self[index(at: value)]
+  }
+}
+
+public extension String {
+  subscript(value: NSRange) -> Substring {
+    self[value.lowerBound..<value.upperBound]
+  }
+}
+
+public extension String {
+  subscript(value: CountableClosedRange<Int>) -> Substring {
+    self[index(at: value.lowerBound)...index(at: value.upperBound)]
+  }
+
+  subscript(value: CountableRange<Int>) -> Substring {
+    self[index(at: value.lowerBound)..<index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeUpTo<Int>) -> Substring {
+    self[..<index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeThrough<Int>) -> Substring {
+    self[...index(at: value.upperBound)]
+  }
+
+  subscript(value: PartialRangeFrom<Int>) -> Substring {
+    self[index(at: value.lowerBound)...]
+  }
+}
+
+private extension String {
+  func index(at offset: Int) -> String.Index {
+    index(startIndex, offsetBy: offset)
+  }
+}
---
+++ WordleCheat/remainingWords.swift
@@ -0,0 +1,53 @@
+//
+//  remainingWords.swift
+//  WordleCheat
+//
+//  Created by Owen Flanagan on 4/25/22.
+//
+
+import Foundation
+
+class remainingWord {
+   
+    
+    var wordList: [String] = []
+    
+    let letterFrequencies = ["A":43, "B": 11, "C":23, "D": 17, "E": 57, "F": 9, "G": 13, "H": 15, "I":38, "J":1,"K":6, "L":28, "M":15, "N":34, "O":37, "P":16, "Q":1, "R":39, "S":29, "T":35, "U":19, "V":5, "W":7, "X":1, "Y":9, "Z":1]
+    //https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
+    
+    func eliminateOptionsGreen(letter: String, position: Int){
+        var temp: [String]
+        temp = wordList.filter{String($0[position]) == letter}
+        wordList = temp
+    }
+    
+    func eliminateOptionsYellow(letter: String){
+        var temp: [String]
+        temp = wordList.filter{$0.contains(letter)}
+        wordList = temp
+    }
+    
+    func bestOption() -> String {
+        var value = 0
+        var bestWord = ""
+        for word in wordList{
+            var tempValue = 0
+            for letter in word{
+                tempValue += letterFrequencies[String(letter)]!
+            }
+            if tempValue > value{
+                bestWord = word
+                value = tempValue
+            }
+        }
+        return bestWord
+    }
+    
+    func setWordList(words: [String]){
+        wordList = words
+    }
+    
+    func numWords() -> Int{
+        return wordList.count
+    }
+}
