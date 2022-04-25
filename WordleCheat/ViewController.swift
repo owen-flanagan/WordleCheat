d2688549a871ba2754f6f22cc7f3c3dd55d45918
owen-flanagan <flanagow@bc.edu>
2022-04-25 17:50:59 -0400

    list of words working

--- WordleCheat/ViewController.swift
+++ WordleCheat/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
     let letterFrequencies = ["A":43, "B": 11, "C":23, "D": 17, "E": 57, "F": 9, "G": 13, "H": 15, "I":38, "J":1,"K":6, "L":28, "M":15, "N":34, "O":37, "P":16, "Q":1, "R":39, "S":29, "T":35, "U":19, "V":5, "W":7, "X":1, "Y":9, "Z":1]
     //https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var wordList = [String]()
+        let path = Bundle.main.path(forResource: "wordleWords.txt", ofType: nil)!
+        let s = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        wordList = s.components(separatedBy: "\n")
+        print ("wordList count = \(wordList.count)")
+        print(wordList)
     }
+    
 
 
 }
